a6e3a205a3abbb29cbc394ea8445532a59473d5e
Yogesh Choudhary <yogesh.choudhary@parentpay.com>
2025-06-17 23:20:02 +0100

    some C# features explored

---
+++ misc-cs-features/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("================");
+Console.WriteLine("Collections");
+Console.WriteLine("================");
+
+List<string> names = ["John", "Jane", "Doe"];
+List<string> moreNames = ["Alice", "Bob", "Charlie"];
+
+Console.WriteLine("Names: " + string.Join(", ", names));
+Console.WriteLine("More Names: " + string.Join(", ", moreNames));
+
+List<string> combinedNames = [.. names, .. moreNames];
+Console.WriteLine("Combined Names: " + string.Join(", ", combinedNames));
+
--- records/Program.cs
+++ records/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<Employee> employees = [];
+
+while (true)
+{
+    Console.WriteLine("Enter your name:");
+    string name = Console.ReadLine() ?? "Unknown";
+    Console.WriteLine("Enter your position:");
+    string position = Console.ReadLine() ?? "Unknown";
+    Guid id = Guid.Empty;
+
+    var employee = new Employee(id, name, position);
+    if (employees.Contains(employee))
+    {
+        Console.WriteLine("Employee already exists in the list.");
+        continue;
+    }
+    else
+    {
+        employees.Add(employee);
+        Console.WriteLine(employee + " added to the list.");
+    }
+
+    Console.WriteLine("Enter 'exit' to quit, 'copy' to clone this employee, or press Enter to continue...");
+    string? input = Console.ReadLine();
+    if (input?.ToLower() == "exit")
+    {
+        break;
+    }
+    else if (input?.ToLower() == "copy")
+    {
+        var clonedEmployee = employee with { Id = Guid.NewGuid() };
+        employees.Add(clonedEmployee);
+        Console.WriteLine(clonedEmployee + " cloned and added to the list.");
+    }
+    else
+    {
+        Console.WriteLine("Continuing...");
+    }
+}
+
+record Employee(Guid Id, string Name, string Position)
+{
+    public override string ToString() => $"{Id} - {Name} - {Position}";
+}
